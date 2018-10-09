8a986ef2e841f1135069cc719877fc9efec39d67
Viktor Ladochkin <victor.ladochkin@onlite.net>
2018-10-09 11:39:51 +0700

    Get consumer key from client params

--- lib/elixero/utils/oauth.ex
+++ lib/elixero/utils/oauth.ex
@@ -20,7 +20,8 @@ defmodule EliXero.Utils.Oauth do
 
     signature = sign(base_string, token)
 
-    "OAuth oauth_signature=\"" <> signature <> "\", " <> EliXero.Utils.Helpers.join_params_keyword(oauth_params, :auth_header)
+    "OAuth oauth_signature=\"" <>
+      signature <> "\", " <> EliXero.Utils.Helpers.join_params_keyword(oauth_params, :auth_header)
   end
 
   def create_auth_header(method, url, additional_params) do
@@ -28,19 +29,23 @@ defmodule EliXero.Utils.Oauth do
 
     signature = sign(base_string)
 
-    "OAuth oauth_signature=\"" <> signature <> "\", " <> EliXero.Utils.Helpers.join_params_keyword(oauth_params, :auth_header)
+    "OAuth oauth_signature=\"" <>
+      signature <> "\", " <> EliXero.Utils.Helpers.join_params_keyword(oauth_params, :auth_header)
   end
 
   defp create_oauth_context(method, url, additional_params) do
-    timestamp = :erlang.float_to_binary(Float.floor(:os.system_time(:milli_seconds) / 1000), [{:decimals, 0}])
+    timestamp =
+      :erlang.float_to_binary(Float.floor(:os.system_time(:milli_seconds) / 1000), [
+        {:decimals, 0}
+      ])
 
     oauth_signing_params = [
-        oauth_consumer_key: oauth_consumer_key(),
-        oauth_nonce: EliXero.Utils.Helpers.random_string(10),
-        oauth_signature_method: signature_method(),
-        oauth_version: "1.0",
-        oauth_timestamp: timestamp
-      ]
+      oauth_consumer_key: additional_params[:oauth_token],
+      oauth_nonce: EliXero.Utils.Helpers.random_string(10),
+      oauth_signature_method: signature_method(),
+      oauth_version: "1.0",
+      oauth_timestamp: timestamp
+    ]
 
     params = additional_params ++ oauth_signing_params
 
@@ -48,8 +53,14 @@ defmodule EliXero.Utils.Oauth do
     url = Enum.at(uri_parts, 0)
 
     params_with_extras =
-      if (length(uri_parts) > 1) do
-        query_params = Enum.at(uri_parts, 1) |> URI.decode_query |> Enum.map(fn({key, value}) -> {String.to_atom(key),  URI.encode_www_form(value) |> String.replace("+", "%20") } end)
+      if length(uri_parts) > 1 do
+        query_params =
+          Enum.at(uri_parts, 1)
+          |> URI.decode_query()
+          |> Enum.map(fn {key, value} ->
+            {String.to_atom(key), URI.encode_www_form(value) |> String.replace("+", "%20")}
+          end)
+
         params ++ query_params
       else
         params
@@ -58,11 +69,13 @@ defmodule EliXero.Utils.Oauth do
     params_with_extras = Enum.sort(params_with_extras)
 
     base_string =
-      method <> "&" <>
-      URI.encode_www_form(url) <> "&" <>
-      URI.encode_www_form(
-        EliXero.Utils.Helpers.join_params_keyword(params_with_extras, :base_string)
-      )
+      method <>
+        "&" <>
+        URI.encode_www_form(url) <>
+        "&" <>
+        URI.encode_www_form(
+          EliXero.Utils.Helpers.join_params_keyword(params_with_extras, :base_string)
+        )
 
     {base_string, params}
   end
@@ -86,7 +99,7 @@ defmodule EliXero.Utils.Oauth do
   defp rsa_sha1_sign(base_string) do
     hashed = :crypto.hash(:sha, base_string)
 
-    {:ok, body} = File.read private_key()
+    {:ok, body} = File.read(private_key())
 
     [decoded_key] = :public_key.pem_decode(body)
     key = :public_key.pem_entry_decode(decoded_key)
@@ -100,8 +113,8 @@ defmodule EliXero.Utils.Oauth do
         nil -> oauth_consumer_secret() <> "&"
         _ -> oauth_consumer_secret() <> "&" <> token["oauth_token_secret"]
       end
+
     signed = :crypto.hmac(:sha, key, base_string)
     URI.encode(Base.encode64(signed), &URI.char_unreserved?(&1))
   end
-
 end
